f5a58d40db78f1a931faccddc6aa41140e8e4eac
oscarvilla <villaoscarad@gmail.com>
2016-11-23 23:26:20 -0500

    countwords with quanteda parallelized working out

--- firstMilestone.R
+++ firstMilestone.R
@@ -1,32 +1,47 @@
+## http://stackoverflow.com/questions/25330753/more-efficient-means-of-creating-a-corpus-and-dtm-with-4m-rows
+
 library(tm)
 library(doMC)
-registerDoMC(cores = 6)
-twitter <- readLines("/home/oscar/Documents/Capstone/en_US/en_US.twitter.txt")
+registerDoMC(cores = 4)
+## Puts the text in a matrix as rows each one
+data <- data.frame(text=readLines("/home/oscar/Documents/DSSCapstone/en_US/en_US.twitter.txt"), 
+                   n = 5000, stringsAsFactors = F)
 
-data <- data.frame(text=twitter, stringsAsFactors = F)
+library(stringi)
+library(SnowballC)
+## This function break all sentences in words: takes each row and break them in columns
+## Now each row is a list
+out <- stri_extract_all_words(stri_trans_tolower(SnowballC::wordStem(data[[1]], "english")))
+## in old package versions it was named 'stri_extract_words'
+## Then get names to each of the cols: 'doc1', 'doc2'...
+names(out) <- paste0("doc", 1:length(out))
+## Separate the lists and the keep just the unique values: make a vector with the unique values
+lev <- sort(unique(unlist(out)))
+## Creates a matrix term x freq in each doc
+dat <- do.call(cbind, lapply(out, function(x, lev) {
+        tabulate(factor(x, levels = lev, ordered = TRUE), nbins = length(lev))
+}, lev = lev))
+
+## Assign the term as row name
+rownames(dat) <- sort(lev)
+
+library(tm)
+## remove stop words as rows
+dat <- dat[!rownames(dat) %in% tm::stopwords("english"), ] 
+
+library(slam)
+dat2 <- slam::as.simple_triplet_matrix(dat)
+
+tdm <- tm::as.TermDocumentMatrix(dat2, weighting=weightTf)
+tdm
+
+## or...
+## dtm <- tm::as.DocumentTermMatrix(dat2, weighting=weightTf)
+## dtm
+##########################################################
+freq <- rowSums(as.matrix(dat))
+barplot(sort(freq, decreasing = T))
+head(sort(freq, decreasing = T))
+Terms(tdm)
 
 
-
-
-
-
-
-
-
-
-max(nchar(docs))
-docs <- Corpus(VectorSource(docs))
-writeLines(as.character(docs[[1000]]))
-#create the toSpace content transformer
-toSpace <- content_transformer(function(x, pattern) {return (gsub(pattern, "", x))})
-
-docs <- tm_map(docs, toSpace, "-")
-docs <- tm_map(docs, toSpace, ":")
-writeLines(as.character(docs[[1000]]))
-############################################################################
-sum(as.numeric(grep("love", blogs))) / sum(as.numeric(grep("hate", blogs)))
-sum(grepl("love", twitter)) / sum(grepl("hate", twitter))
-twitter[which(grepl("biostats", twitter))]
-twitter[[556872]]
-sum(grepl("A computer once beat me at chess, but it was no match for me at kickboxing", 
-      twitter))
--- firstMilestoneQuanteda.R
+++ firstMilestoneQuanteda.R
@@ -1,7 +1,26 @@
 require(quanteda)
 library(doMC)
-registerDoMC(cores = 6)
-mytext <- readLines("/home/oscar/Documents/Capstone/en_US/en_US.twitter.txt")
-dfm <- dfm(mytext, ignoredFeatures = stopwords("english"), stem = TRUE)
-## The elapsed time for this dfm was 389 seconds, around 6.48 minutes.
-## That's no so bad
+library(text2vec)
+registerDoMC(cores = 4)
+mytext <- readLines("/home/oscar/Documents/DSSCapstone/en_US/en_US.twitter.txt")
+splits <- split_into(mytext, 20)
+
+library(foreach)
+library(doParallel)
+
+f <- 0
+s1 <- Sys.time()
+finalMatrix <- foreach(i=1:length(splits), .packages = c("quanteda")) %dopar% {
+        dfm <- dfm(as.character(splits[i]), ignoredFeatures = stopwords("english"), stem = TRUE)
+        f <- f + colSums(dfm)
+}
+s2 <- Sys.time()
+s2 - s1
+
+countWords <- 0
+for(i in 1:length(finalMatrix)) {
+        countWords <- finalMatrix[[i]] + countWords
+}
+countWords <- sort(countWords, decreasing = T)
+barplot(countWords[1:10])
+## Now, I have to compare this countword with the deployed by the function quanteda by itself
